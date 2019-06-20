ba598212d9a6715b2db11478179283a56e7c4ff4
Erik Sthal <erik.sthal@gmail.com>
2019-06-19 19:34:46 -0700

    Fix uniforms bug

--- client/src/constants/uniforms.js
+++ client/src/constants/uniforms.js
@@ -55,6 +55,31 @@ const uniformsTemplate = {
     ],
     shoes: `Shoes: Puma Classic Black & White Suede shoes – available at Vancouver
     Dance Supply.`
+  },
+  balletTap: {
+    title: "Ballet/Tap Uniform:",
+    levels: [
+      `Ballet:`,
+      `All Levels: Snug fitting DANCE CO Bodysuit & Pink Sansha Tights.`,
+      `Preschool, Pre-Primary: DANCE CO Pink & White Raglan or Boatneck
+      Bodysuit, DANCE CO Pink & White Skirt (Skirt should sit on waist - not
+      hips, with the bottom of the skirt in line with the bottom of the
+      fingertips. Correct sizing is required to use the skirt properly in
+      class.)`,
+      `Primary: DANCE CO Black & Pink Raglan, DANCE CO Black & Pink Skirt`,
+      `Recreational/Demi-Comp Junior: DANCE CO Black Bodysuit, DANCE CO Black
+      & White Skirt`,
+      `Recreational/Demi-Comp Intermediate & Senior: DANCE CO Black Bodysuit,
+      DANCE CO White Waist Elastic (This provides teachers with an enhanced
+      view of dancer alignment)`,
+      `Boys: Fitted white DANCE CO bodysuit & DANCE CO shorts or leggings.`,
+      `Tap:`,
+      `DANCE CO fitted leggings or shorts & a DANCE CO well fitted top or
+      bodysuit.`
+    ],
+    shoes: ` Ballet Shoes: Snug fitting Canvas Ballet shoe ("Star-split" 15C, "Pro" 1C or
+    "Stretch One"), Tap Shoes: Well fitting Sansha Black Leather Tap Shoe T-Moravia Junior
+    Intensive 2`
   }
 };
 
@@ -67,5 +92,5 @@ export const uniforms = {
   "Musical Theatre": uniformsTemplate.jazz,
   Breakdance: uniformsTemplate.hiphop,
   "Hip Hop": uniformsTemplate.hiphop,
-  "Ballet/Tap": `${uniformsTemplate.ballet} ${uniformsTemplate.tap}`
+  "Ballet/Tap": uniformsTemplate.balletTap
 };
