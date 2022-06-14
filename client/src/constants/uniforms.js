bc6d1fcdc43229026ce9cc343a6399e273db7293
Erik Sthal <erik.sthal@gmail.com>
2022-06-14 12:05:10 -0700

    Fix UTF-8 for uniforms

--- client/src/constants/uniforms.js
+++ client/src/constants/uniforms.js
@@ -6,16 +6,16 @@ const uniformsTemplate = {
       `Pre-primary: DANCE CO Pink Bodysuit, DANCE CO Pink Skirt`,
       `Primary: DANCE CO Pink Bodysuit, DANCE CO Pink Skirt`,
       `Junior: DANCE CO Black Bodysuit, DANCE CO Black Skirt`,
-      `Intermediate and Senior: DANCE CO Black Bodysuit &amp; optional black skirt`,
-      `Boys: Fitted white DANCE CO bodysuit &amp; DANCE CO shorts`,
+      `Intermediate and Senior: DANCE CO Black Bodysuit & optional black skirt`,
+      `Boys: Fitted white DANCE CO bodysuit & DANCE CO shorts`,
     ],
-    shoes: ` Shoes: Snug fitting Canvas Ballet shoe (&quot;Star-split&quot; 15C, &quot;Pro&quot; 1C or &quot;Stretch
-      One&quot;)`,
+    shoes: ` Shoes: Snug fitting Canvas Ballet shoe ("Star-split" 15C, "Pro" 1C or "Stretch
+      One")`,
   },
   jazz: {
     title: "JAZZ, LYRICAL, STAGE & MUSICAL THEATRE Uniform:",
     levels: [
-      `All levels: DANCE CO fitted leggings or shorts &amp; a DANCE CO well-fitted top or
+      `All levels: DANCE CO fitted leggings or shorts & a DANCE CO well-fitted top or
       bodysuit.`,
     ],
     shoes: `Shoes: Black Sansha Jazz shoe (Shoes for lyrical and stage will be determined
@@ -24,7 +24,7 @@ const uniformsTemplate = {
   acro: {
     title: `CONTEMPORARY Uniform:`,
     levels: [
-      `All levels: DANCE CO fitted leggings or shorts &amp; a DANCE CO well-fitted top or
+      `All levels: DANCE CO fitted leggings or shorts & a DANCE CO well-fitted top or
       bodysuit.`,
     ],
     shoes: `Shoes: Bare feet. Dance Socks, Pirouette Turning Shoes, or Foot Undeez may
@@ -33,7 +33,7 @@ const uniformsTemplate = {
   tumbling: {
     title: `Acro & TUMBLING Uniform:`,
     levels: [
-      `All levels: DANCE CO fitted leggings or shorts &amp; a DANCE CO well-fitted top or
+      `All levels: DANCE CO fitted leggings or shorts & a DANCE CO well-fitted top or
       bodysuit.`,
     ],
     shoes: `Shoes: Bare feet.`,
@@ -41,14 +41,14 @@ const uniformsTemplate = {
   aerialSilks: {
     title: `AERIAL SILKS Uniform:`,
     levels: [
-      `All levels: DANCE CO fitted leggings or shorts &amp; a DANCE CO well-fitted bodysuit.`,
+      `All levels: DANCE CO fitted leggings or shorts & a DANCE CO well-fitted bodysuit.`,
     ],
     shoes: `Shoes: Bare feet.`,
   },
   tap: {
     title: `TAP Uniform:`,
     levels: [
-      `All levels: DANCE CO fitted leggings or shorts &amp; a DANCE CO well-fitted top or
+      `All levels: DANCE CO fitted leggings or shorts & a DANCE CO well-fitted top or
       bodysuit.`,
     ],
     shoes: `Shoes: Sansha Black Leather Tap Shoe`,
@@ -56,31 +56,21 @@ const uniformsTemplate = {
   hiphop: {
     title: `HIP HOP & BREAKDANCE Uniform:`,
     levels: [
-      `Preschool - Senior: DANCE CO hip hop pants or shorts & choice of Dance
-    Co hiphop shirt. DANCE CO Sleeveless Crop Top and Cross Back Top MUST
-    be worn over a DANCE CO sports bra or bodysuit.`,
+      `All levels: DANCE CO hip hop pants or shorts & choice of Dance Co hip hop
+      shirt`,
     ],
-    shoes: `Shoes: Puma Classic Black & White Suede shoes – available at Vancouver
-    Dance Supply.`,
+    shoes: `Shoes: Puma Classic Black & White Suede shoes - available at Vancouver
+    Dance Supply`,
   },
   balletTap: {
     title: "Ballet/Tap Uniform:",
     levels: [
       `Preschool: DANCE CO Pink Bodysuit with attached skirt`,
       `Pre-Primary: DANCE CO Pink Bodysuit, DANCE CO Pink Skirt`,
-      `Boys: Fitted white DANCE CO bodysuit &amp; DANCE CO shorts`,
+      `Boys: Fitted white DANCE CO bodysuit & DANCE CO shorts`,
     ],
-    shoes: ` Ballet: Snug fitting Canvas Ballet shoe (&quot;Star-split&quot; 15C, &quot;Pro&quot; 1C or &quot;Stretch
-      One&quot;), Tap: Sansha Black Leather Tap Shoe`,
-  },
-  hiphopBreakdance: {
-    title: `HIP HOP & BREAKDANCE Uniform:`,
-    levels: [
-      `All levels: DANCE CO hip hop pants or shorts &amp; choice of Dance Co hip hop
-      shirt`,
-    ],
-    shoes: `Shoes: Puma Classic Black &amp; White Suede shoes - available at Vancouver
-    Dance Supply`,
+    shoes: ` Ballet: Snug fitting Canvas Ballet shoe ("Star-split" 15C, "Pro" 1C or "Stretch
+      One"), Tap: Sansha Black Leather Tap Shoe`,
   },
 };
 
@@ -97,5 +87,5 @@ export const uniforms = {
   "Hip Hop": uniformsTemplate.hiphop,
   "Ballet/Tap": uniformsTemplate.balletTap,
   Lyrical: uniformsTemplate.jazz,
-  "Hip Hop/Breakdance": uniformsTemplate.hiphopBreakdance,
+  "Hip Hop/Breakdance": uniformsTemplate.hiphop,
 };
